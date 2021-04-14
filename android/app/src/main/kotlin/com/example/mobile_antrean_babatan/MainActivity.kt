d36c68c27ea360090fe3084e3b8283818cfb6baa
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2021-04-15 05:03:23 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/mobile_antrean_babatan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_antrean_babatan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Antrean Pasien Babatan"),
+      ),
+    );
+  }
+}
