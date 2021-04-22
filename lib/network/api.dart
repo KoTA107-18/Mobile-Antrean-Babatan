311cf823a9cc3e7847a8452b55b4193a1acb9d1e
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2021-04-22 19:38:49 +0700

    Network API, HTTP Request, POST Register

--- lib/network/api.dart
+++ lib/network/api.dart
@@ -2,18 +2,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class RequestApi {
-  final String apiUrl = "rest-api-babatan.herokuapp.com";
-  Future<List<dynamic>> getPasien() async {
+  static final String apiUrl = "rest-api-babatan.herokuapp.com";
+  static Future<List<dynamic>> getPasien() async {
     var result = await http.get(Uri.http(apiUrl, 'pasien'));
     return json.decode(result.body)['data'];
   }
 
-  Future<bool> registerPasien() async {
+  static Future<bool> registerPasien() async {
     var result = await http.post(Uri.http(apiUrl, 'pasien'));
-    if (result.statusCode == 201) {
-      return true;
-    } else {
-      return false;
-    }
+    //print(json.decode(result.body)['response']);
+    print(result.body);
+    return true;
   }
 }
