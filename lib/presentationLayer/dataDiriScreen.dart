ff13e65a8b2f0a980056ebaa12ed8dd65003e4b8
Andi Fauzy Dewantara <andifauzy00@gmail.com>
2021-07-17 14:29:44 +0700

    UI Edit Profil dan ubah password.

--- lib/presentationLayer/dataDiriScreen.dart
+++ lib/presentationLayer/dataDiriScreen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:mobile_antrean_babatan/utils/color.dart';
+import 'package:mobile_antrean_babatan/utils/textFieldModified.dart';
 
 class DataDiriScreen extends StatefulWidget {
   @override
@@ -6,11 +10,392 @@ class DataDiriScreen extends StatefulWidget {
 }
 
 class _DataDiriScreenState extends State<DataDiriScreen> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController _namaLengkap = TextEditingController();
+  TextEditingController _tglLahir = TextEditingController();
+  TextEditingController _alamat = TextEditingController();
+  TextEditingController _kepalaKeluarga = TextEditingController();
+  TextEditingController _username = TextEditingController();
+  TextEditingController _nomorHandphone = TextEditingController();
+  bool isClickValidated = false;
+  bool isVerifiedNumber = false;
+  DateTime selectedDate = DateTime.now();
+  bool isEnabled = false;
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(1945),
+        lastDate: DateTime.now());
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+
+  void verifiedInput() {
+    setState(() {
+      isClickValidated = true;
+    });
+    if (_formKey.currentState.validate()) {
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Halaman Data Diri"),
+      body: Container(
+        color: ColorTheme.greenDark,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Data Diri Anda"),
+          ),
+          body: Form(
+            autovalidateMode: (isClickValidated)
+                ? AutovalidateMode.onUserInteraction
+                : AutovalidateMode.disabled,
+            key: _formKey,
+            child: ListView(
+              children: [
+                Container(
+                    padding:
+                        EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(16.0),
+                          child: Image(
+                            height: MediaQuery.of(context).size.width / 3,
+                            image: AssetImage('asset/Avatar.png'),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              "Edit Profil",
+                              style: TextStyle(
+                                  color: ColorTheme.greenDark,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Switch(
+                              value: isEnabled,
+                              onChanged: (value) {
+                                setState(() {
+                                  isEnabled = value;
+                                  print(isEnabled);
+                                });
+                              },
+                              activeTrackColor: ColorTheme.greenLight,
+                              activeColor: ColorTheme.greenDark,
+                            ),
+                          ],
+                        ),
+                        textFieldModified(
+                            label: 'Nama Lengkap',
+                            hint: 'Isi nama anda',
+                            icon: Icon(Icons.person),
+                            isEnabled: isEnabled,
+                            formatter: [
+                              FilteringTextInputFormatter.allow(
+                                  RegExp('[a-zA-Z ]')),
+                            ],
+                            validatorFunc: (value) {
+                              if (value.isEmpty) {
+                                return "Harus diisi";
+                              } else {
+                                return null;
+                              }
+                            },
+                            controller: _namaLengkap),
+                        SizedBox(height: 20.0),
+                        Row(
+                          children: [
+                            Flexible(
+                              child: textFieldModified(
+                                  isEnabled: false,
+                                  label: 'Tanggal Lahir',
+                                  icon: Icon(Icons.date_range),
+                                  controller: _tglLahir),
+                            ),
+                            SizedBox(width: 16.0),
+                            ElevatedButton(
+                                onPressed: () {
+                                  if (isEnabled) {
+                                    _selectDate(context).then((value) {
+                                      setState(() {
+                                        _tglLahir.text =
+                                            "${selectedDate.year.toString()}-${selectedDate.month.toString().padLeft(2, '0')}-${selectedDate.day.toString().padLeft(2, '0')}";
+                                      });
+                                    });
+                                  } else {
+                                    Fluttertoast.showToast(
+                                        msg:
+                                            "Nyalakan Edit Profil Terlebih Dahulu.",
+                                        toastLength: Toast.LENGTH_SHORT);
+                                  }
+                                },
+                                child: Icon(Icons.date_range))
+                          ],
+                        ),
+                        SizedBox(height: 20.0),
+                        textFieldModified(
+                            label: 'Alamat Lengkap',
+                            hint: 'Isi alamat rumah anda',
+                            icon: Icon(Icons.map),
+                            isEnabled: isEnabled,
+                            validatorFunc: (value) {
+                              if (value.isEmpty) {
+                                return "Harus diisi";
+                              } else {
+                                return null;
+                              }
+                            },
+                            controller: _alamat),
+                        SizedBox(height: 20.0),
+                        textFieldModified(
+                            label: 'Nama Kepala Keluarga',
+                            hint: 'Isi nama kepala keluarga anda',
+                            icon: Icon(Icons.person),
+                            isEnabled: isEnabled,
+                            formatter: [
+                              FilteringTextInputFormatter.allow(
+                                  RegExp('[a-zA-Z ]')),
+                            ],
+                            validatorFunc: (value) {
+                              if (value.isEmpty) {
+                                return "Harus diisi";
+                              } else {
+                                return null;
+                              }
+                            },
+                            controller: _kepalaKeluarga),
+                        SizedBox(height: 20.0),
+                        textFieldModified(
+                            label: 'Username',
+                            hint: 'Kombinasi huruf dan angka.',
+                            icon: Icon(Icons.person),
+                            isEnabled: isEnabled,
+                            formatter: [
+                              FilteringTextInputFormatter.allow(
+                                  RegExp('[a-zA-Z0-9]')),
+                            ],
+                            validatorFunc: (value) {
+                              if (value.isEmpty) {
+                                return "Harus diisi";
+                              } else if (value.length < 4) {
+                                return "Minimum 4 karakter";
+                              } else {
+                                return null;
+                              }
+                            },
+                            controller: _username),
+                        SizedBox(height: 20.0),
+                        textFieldModified(
+                            label: 'Nomor Seluler',
+                            hint: 'Contoh : 6281122224444',
+                            icon: Icon(Icons.call),
+                            typeKeyboard: TextInputType.number,
+                            isEnabled: isEnabled,
+                            formatter: [
+                              FilteringTextInputFormatter.allow(
+                                  RegExp(r'[0-9]')),
+                            ],
+                            validatorFunc: (value) {
+                              if (value.isEmpty) {
+                                return "Harus diisi";
+                              } else if (value.length < 10) {
+                                return "Minimum 10 digit";
+                              } else if (value.substring(0, 2) != "62") {
+                                return "Format tidak sesuai, awali dengan 62";
+                              } else {
+                                return null;
+                              }
+                            },
+                            controller: _nomorHandphone),
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 16.0, horizontal: 4.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'Ingin ubah Password?',
+                                style: TextStyle(fontFamily: 'Montserrat'),
+                              ),
+                              SizedBox(width: 5.0),
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              UbahPasswordScreen()));
+                                },
+                                child: Text(
+                                  'Ubah disini',
+                                  style: TextStyle(
+                                      color: ColorTheme.greenDark,
+                                      fontWeight: FontWeight.bold,
+                                      decoration: TextDecoration.underline),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {
+                            if(isEnabled){
+                              verifiedInput();
+                            } else {
+                              Fluttertoast.showToast(
+                                  msg:
+                                  "Nyalakan Edit Profil Terlebih Dahulu.",
+                                  toastLength: Toast.LENGTH_SHORT);
+                            }
+                          },
+                          child: Container(
+                            height: 40.0,
+                            child: Material(
+                              borderRadius: BorderRadius.circular(20.0),
+                              color: (isEnabled)
+                                  ? ColorTheme.greenDark
+                                  : Colors.grey,
+                              elevation: 7.0,
+                              child: Center(
+                                child: Text(
+                                  'Ubah Data Diri',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 24.0),
+                      ],
+                    )),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class UbahPasswordScreen extends StatefulWidget {
+  @override
+  _UbahPasswordScreenState createState() => _UbahPasswordScreenState();
+}
+
+class _UbahPasswordScreenState extends State<UbahPasswordScreen> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController _password = TextEditingController();
+  TextEditingController _passwordTwo = TextEditingController();
+  bool isClickValidated = false;
+
+  void verifiedInput() {
+    setState(() {
+      isClickValidated = true;
+    });
+    if (_formKey.currentState.validate()) {
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: ColorTheme.greenDark,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Ubah Password"),
+          ),
+          body: Form(
+            autovalidateMode: (isClickValidated)
+                ? AutovalidateMode.onUserInteraction
+                : AutovalidateMode.disabled,
+            key: _formKey,
+            child: ListView(
+              children: [
+                Container(
+                    padding:
+                        EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                    child: Column(
+                      children: <Widget>[
+                        textFieldModified(
+                            label: 'Password',
+                            hint: 'Isi password anda',
+                            icon: Icon(Icons.vpn_key),
+                            formatter: [
+                              FilteringTextInputFormatter.deny(RegExp('[ ]')),
+                            ],
+                            isPasword: true,
+                            validatorFunc: (value) {
+                              if (value.isEmpty) {
+                                return "Harus diisi";
+                              } else if (value.length < 4) {
+                                return "Minimum 4 karakter";
+                              } else {
+                                return null;
+                              }
+                            },
+                            controller: _password),
+                        SizedBox(height: 20.0),
+                        textFieldModified(
+                            label: 'Konfirmasi Password',
+                            hint: 'Isi kembali password anda',
+                            icon: Icon(Icons.vpn_key),
+                            formatter: [
+                              FilteringTextInputFormatter.deny(RegExp('[ ]')),
+                            ],
+                            isPasword: true,
+                            validatorFunc: (value) {
+                              if (value.isEmpty) {
+                                return "Harus diisi";
+                              } else if (value.length < 4) {
+                                return "Minimum 4 karakter";
+                              } else if (value != _password.text) {
+                                return "Konfirmasi Password tidak sesuai";
+                              } else {
+                                return null;
+                              }
+                            },
+                            controller: _passwordTwo),
+                        SizedBox(height: 20.0),
+                        InkWell(
+                          onTap: () {
+                            verifiedInput();
+                          },
+                          child: Container(
+                            height: 40.0,
+                            child: Material(
+                              borderRadius: BorderRadius.circular(20.0),
+                              color: ColorTheme.greenDark,
+                              elevation: 7.0,
+                              child: Center(
+                                child: Text(
+                                  'Ubah Password',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 24.0),
+                      ],
+                    )),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
